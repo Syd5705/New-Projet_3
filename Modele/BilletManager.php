d2c95d9f3ffcb98706580c968f0e3aebc7130949
Syd5705 <32962965+Syd5705@users.noreply.github.com>
2017-11-18 15:18:11 +0100

    Add files via upload

--- Modele/BilletManager.php
+++ Modele/BilletManager.php
@@ -27,7 +27,6 @@ class BilletManager extends Modele {
             $billets[]=$billet;  /** on stock l'instance $billet */
         }
 
-
         return $billets;
     }
 
@@ -37,9 +36,10 @@ class BilletManager extends Modele {
      * return array Le billet
      * throws Exception Si l'identifiant du billet est inconnu
      */
-    public function getBillet($idBillet) {
+    public function getBillet($idBillet) 
+    {
         $sql = 'select BIL_ID as id, BIL_DATE as date,'
-                . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+                . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'  // la concaténation sert à ne pas mettre à la ligne
                 . ' where BIL_ID=?';
         $billet = $this->executerRequete($sql, array($idBillet));
         if ($billet->rowCount() > 0)
@@ -48,12 +48,43 @@ class BilletManager extends Modele {
             throw new Exception("Aucun billet ne correspond à l'identifiant '$idBillet'");
     }
 
+    public function enregistrerBillet($titre, $contenu, $idBillet)
+    {
+    $sql = 'update T_BILLET SET BIL_TITRE = :BIL_TITRE, BIL_CONTENU = :BIL_CONTENU WHERE BIL_ID = :BIL_ID';
+                                  // avec update on met "set" et non "value"
+        $this->executerRequete($sql, array(':BIL_TITRE' => $titre, ':BIL_CONTENU' => $contenu, ':BIL_ID' => $idBillet));  // pour dissocier la variable de la table et celle qu'on passe en parametre
+    }
+    
+    public function SupprimerBillet($idBillet)
+    {
+    $sql = 'delete FROM T_BILLET WHERE BIL_ID = :BIL_ID';
+                
+      $this->executerRequete($sql, array(':BIL_ID' => $idBillet));  // Va supprimer toute la ligne avec l'id qu'on a passé en parametre car l'ID est une clé primaire          
+               
+    }
 
-// public function SupprimerBillets (){
+    public function CreationBillet($titre, $contenu, $idBillet)
+    {
+     $sql = 'insert into T_BILLET (BIL_TITRE, BIL_CONTENU, BIL_ID VALUES(BIL_TITRE, BIL_CONTENU, BIL_ID)';
+     $this->executerRequete($sql, array(':BIL_TITRE' => $titre, ':BIL_CONTENU' => $contenu, ':BIL_ID' => $idBillet));  
+    }
 
-//      $sql = 'delete BIL_ID as id, BIL_DATE as date,'
-//              . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
-//            . ' order by BIL_ID desc';
-// }
+
+ // $sql = 'insert into T_BILLET (BIL_TITRE, BIL_CONTENU, BIL_ID VALUES(:BIL_TITRE, BIL_CONTENU, BIL_ID)'; ce que j'avais à la base
+ // $sql = 'insert into T_BILLET SET BIL_TITRE = :BIL_TITRE, BIL_CONTENU = :BIL_CONTENU, BIL_ID = :BIL_ID'; essai 1
+
+ public function getCommentaires($idCommentaire, $auteur, $contenu, $date) 
+    {
+        $sql = 'select COM_ID as id, COM_DATE as date,'
+                . ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE'  // la concaténation sert à ne pas mettre à la ligne
+                . ' where COM_ID=?';
+        $Commentaire = $this->executerRequete($sql, array($idCommentaire, $auteur, $contenu, $date));
+        if ($Commentaire->rowCount() > 0)
+            return $Commentaire->fetch();  // Accès à la première ligne de résultat
+        else
+            throw new Exception("Aucun Commentaire ne correspond à l'identifiant '$idCommentaire'");
+    }
+
 
 }
+
--- Modele/CommentaireManager.php
+++ Modele/CommentaireManager.php
@@ -14,11 +14,22 @@ class CommentaireManager extends Modele {
     }
 
     // Ajoute un commentaire dans la base
-    public function ajouterCommentaire($auteur, $contenu, $idBillet) {
+    public function ajouterCommentaire($auteur, $contenu, $idBillet, $date) {
         $sql = 'insert into T_COMMENTAIRE(COM_DATE, COM_AUTEUR, COM_CONTENU, BIL_ID)'
             . ' values(?, ?, ?, ?)';
         $date = date("Y-m-d H:i:s");  // Récupère la date courante
-        $this->executerRequete($sql, array($date, $auteur, $contenu, $idBillet));
+        $this->executerRequete($sql, array($auteur, $contenu, $idBillet, $date));
     }
+
+    // Affiche tous les commentaires --------------------------------------------------------NE FCT PAS
+   // public function listerCommentaires()
+   // {
+  //       $sql = 'select COM_ID as id, COM_DATE as date,'
+  //              . ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE';
+  //      $commentaires = $this->executerRequete($sql, array());
+  //      return $commentaires;
+  //  }
+
+
 }
 
