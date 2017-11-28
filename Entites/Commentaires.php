5924ff920e2e71d8a20c872940011433fa09bc88
Syd5705 <32962965+Syd5705@users.noreply.github.com>
2017-11-28 19:34:25 +0100

    Add files via upload

--- Entites/Commentaires.php
+++ Entites/Commentaires.php
@@ -3,6 +3,7 @@
 require_once 'Modele/Modele.php';
 
 
+
 class Commentaires extends Modele 
 {
 
@@ -26,16 +27,5 @@ class Commentaires extends Modele
      * return array Le commentaire
      * throws Exception Si l'identifiant du commentaire est inconnu
      */
-    public function getCommentaires($idBillet) 
-    {
-        $sql = 'select COM_ID as id, COM_DATE as date,'
-                . ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE'
-                . ' where COM_ID=?';
-        $Commentaire = $this->executerRequete($sql, array($idCommentaire));
-        if ($Commentaire->rowCount() > 0)
-            return $Commentaire->fetch();  // Accès à la première ligne de résultat
-        else
-            throw new Exception("Aucun commentaire ne correspond à l'identifiant '$idCommentaire'");
-    }
-
+   
 }
